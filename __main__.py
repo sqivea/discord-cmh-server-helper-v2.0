5e40fb25c52cf51352008fc733146187854d012f
mavedev <usatchoff@gmail.com>
2020-05-15 22:49:29 +0300

    Bot running provided

--- __main__.py
+++ __main__.py
@@ -1,8 +1,12 @@
 import env  # noqa, used for loading the environment variables.
 
+from os import getenv
+
+from app import CMHBot as Bot
+
 
 def main() -> None:
-    pass
+    Bot().run(getenv('BOT_TOKEN'))
 
 
 if __name__ == '__main__':
---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from .bot import CMHBot
+
+__all__ = [
+    'CMHBot'
+]
