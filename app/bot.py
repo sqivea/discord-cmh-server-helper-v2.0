001cf6cf9692a4fd048018e68c8cf2667f16836a
mavedev <usatchoff@gmail.com>
2020-05-15 22:23:34 +0300

    Message processing provided

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,15 @@
 from singletons import Singleton
 from discord import Client as DiscordClient
+from discord.message import Message
 
 
 class CMHBot(DiscordClient, metaclass=Singleton):
-    pass
+    def __init__(self, **options) -> None:
+        super().__init__(**options)
+
+        @self.event
+        async def on_message(message: Message) -> None:
+            await self._process(message)
+
+    async def _process(self, message: Message) -> None:
+        pass
