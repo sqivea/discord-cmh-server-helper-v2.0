9ee1a40fe11e1f3d947e3cc5f97ca27029643f91
mavedev <usatchoff@gmail.com>
2020-05-17 14:36:14 +0300

    The bot messages transalted

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,8 @@
+from .lang_controller import LangController
+
+
 # String representations for replies.
 class Replies:
-    ON_PING = '`Pong`'
-    ON_DIE = '`CMH Server Helper stopped`'
-    ON_INFO = '`CMH Server Helper v2.0`'
+    ON_PING = '`{}`'.format(LangController.translator['ON_PING'])
+    ON_DIE = '`{}`'.format(LangController.translator['ON_DIE'])
+    ON_INFO = '`{}`'.format(LangController.translator['ON_INFO'])
